ba3fe035b7fdf195a4027965a80fe522809281d9
Karim Alibhai <k4rim.sa@gmail.com>
2014-12-17 17:37:10 -0500

    fix testing procedure on windows

--- make.bat
+++ make.bat
@@ -25,18 +25,19 @@ GOTO :EOF
 
 :TEST
 ECHO.Compiling headers ...
-ECHO.#ifndef _LIBVEX_H>libvex.h
-ECHO.#define _LIBVEX_H>>libvex.h
-ECHO.#define __TEST_LV__>>libvex.h
-ECHO.#include "test.h">>libvex.h
-FOR /F "tokens=*" %%F in ('dir /b include\*.h') DO TYPE include\%%F >>libvex.h
-ECHO.#endif>>libvex.h
-
+ECHO.#ifndef _LIBVEX_H>test\libvex.h
+ECHO.#define _LIBVEX_H>>test\libvex.h
+ECHO.#define __TEST_LV__>>test\libvex.h
+ECHO.#include "test.h">>test\libvex.h
+FOR /F "tokens=*" %%F in ('dir /b include\*.h') DO TYPE include\%%F >>test\libvex.h
+ECHO.#endif>>test\libvex.h
 ECHO.Compiling source ...
-ECHO.#include "libvex.h">libvex.c
-ECHO.#ifdef _LIBVEX_H>>libvex.c
-FOR /F "tokens=*" %%F in ('dir /b src\*.c') DO TYPE src\%%F >>libvex.c
-ECHO.#endif>>libvex.c
+ECHO.#include "libvex.h">test\libvex.c
+ECHO.#ifdef _LIBVEX_H>>test\libvex.c
+FOR /F "tokens=*" %%F in ('dir /b src\*.c') DO TYPE src\%%F >>test\libvex.c
+ECHO.#endif>>test\libvex.c
+ECHO.Compiling object files ...
+gcc -Itest -c test/libvex.c -o test/libvex.o -Wall
 GOTO :EOF
 
 :DOC
